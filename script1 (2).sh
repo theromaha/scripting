b1cf40ad934f694c78c061dfd7587cf806c9a6b5
theromaha <61563358+theromaha@users.noreply.github.com>
2021-03-26 19:29:28 +0200

    Add files via upload

---
+++ script1 (2).sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+p2=$(python2 --version 2>&1)
+p3=$(python3 --version)
+echo $p3 > vers3.txt
+
+num2=2
+num3=$(cut -c 8 vers3.txt)
+
+echo "In your laptop you have a $p2 version and a $p3 version." > test1.txt
+
+if [ $num3 -gt $num2 ]
+then
+echo -e "Python$num3 is installed on your device and I reccomend using it.\nThat's because it's more provising to use in 2021. " >> test1.txt
+else
+echo "You use a python$num2. Download python3 for this link - https://www.python.org/downloads/ " >> test1.txt
+fi
+
+
